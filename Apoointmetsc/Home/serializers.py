ffeb681f52cdbba8f000e94535cc96cd8e8df626
arpitsha26 <arpitsharma1263@gmail.com>
2024-11-07 13:01:25 +0530

    created: serilaizers

---
+++ Apoointmetsc/Home/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Customuser, Doctor, Patient, Admin, Appointment
+
+class CustomuserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customuser
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number','password']
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'specialization','password']
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number', 'Illness','password']
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone_number','password']
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    patient = PatientSerializer(read_only=True)
+    doctor = DoctorSerializer(read_only=True)
+
+    class Meta:
+        model = Appointment
+        fields = ['id', 'patient', 'doctor', 'appointment_date', 'status']
